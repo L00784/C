95f4379c76bc7d57d0ce31eb2bf967c0b9ffbf28
朱彦瑾 <18043543248@163.com>
2024-08-20 20:23:17 +0800

    first commit

---
+++ Test/Test.c
@@ -0,0 +1,14 @@
+/*
+ * @Author: 朱彦瑾 18043543248@163.com
+ * @Date: 2024-08-20 20:16:11
+ * @LastEditors: 朱彦瑾 18043543248@163.com
+ * @LastEditTime: 2024-08-20 20:18:39
+ * @FilePath: \C\Test\Test.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
